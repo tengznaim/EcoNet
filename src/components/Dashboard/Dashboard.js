6037b82909a08998867ca1b42c7931f7bc980cfe
Tengku Naim <naimtmas@gmail.com>
2020-11-21 18:23:41 +0800

    continued work on dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import "../Dashboard/Dashboard.css";
-import { FaHome } from 'react-icons/fa';
+import { AiOutlineWallet } from 'react-icons/ai';
+import { AiOutlineMessage, AiOutlineHome } from 'react-icons/ai';
+import { VscAccount } from 'react-icons/vsc';
+import {Link} from 'react-router-dom';
 
-function Dashboard(){
+
+function Dashboard(props){
+
+    function handleLogout(){
+        props.history.push("/");
+    }
+    
     return(
         <div>
             <div className="dashboardMain">
                 <div className="leftPanel">
-                    <FaHome/>
+                    <Link to="/dashboard" className="links currentPage">
+                        <AiOutlineHome className="icons"/>
+                    </Link>
+                    <AiOutlineWallet className="icons"/>
+                    <AiOutlineMessage className="icons"/>
+                    <VscAccount className="icons"/>
                 </div>
                 <div className="rightContent">
+                    <button className="btn" id="logout" onClick={handleLogout}>Logout</button>
+                    <h1 id="homeText">What would you like to recycle today?</h1>
                 </div>
             </div>
         </div>
